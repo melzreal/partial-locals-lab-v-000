12bd770bab094d1a264c9c8d9bb92d261ff491d2
Melissa Real <melissa.real@hotmail.com>
2019-02-04 16:28:07 +0000

    Automatically backed up by Learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -3,7 +3,7 @@ class ClassroomsController < ApplicationController
   def show
 
     @classroom = Classroom.find(params[:id])
-    @students = @classroom.students
+    @student = @classroom.students
   end
 
   def index
