78f410dd9761b7b02b219e94e92f249084d753bb
Melissa Real <melissa.real@hotmail.com>
2019-02-04 15:37:15 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,8 +9,20 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
+require 'pry'
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(n)
+    names = []
+    if n.empty?
+      all
+    else
+      self.all.map{|m| m.name.include?(n) }
+    end
+  end
+
+
+
 end
